a4fdaed59abb71ec054fa9f1329f13628d2ab18e
Mrtao233 <2157381839@qq.com>
2022-10-25 02:41:54 +0800

    Site updated: 2022-10-25 02:41:54

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+	{
+        name: '不为谁作的歌',     
+        artist: '林俊杰',  
+        url: 'http://music.163.com/song/media/outer/url?id=486194136.mp3',  
+        cover: 'https://baike.baidu.com/pic/TY/55992450/1/f9dcd100baa1cd11728b1181ef40dffcc3cec3fd874a?fr=lemma&fromModule=lemma_top-image&ct=single#aid=1&pic=f9dcd100baa1cd11728b1181ef40dffcc3cec3fd874a' 
+    }
+	]
+});
